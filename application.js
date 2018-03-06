95aa3ca281746684413305946f18804c2dbd6ee2
kingMd21 <36416134+kingMd21@users.noreply.github.com>
2018-03-06 11:23:21 -0500

    Create application.js

---
+++ application.js
@@ -0,0 +1,51 @@
+var config = {
+    apiKey: "AIzaSyC1P4Y5BAMXFrjpXCwJyGZ0xRvDg23Y78k",
+    authDomain: "kingmd21-d6cda.firebaseapp.com",
+    databaseURL: "https://kingmd21-d6cda.firebaseio.com",
+    projectId: "kingmd21-d6cda",
+    storageBucket: "kingmd21-d6cda.appspot.com",
+    messagingSenderId: "737173881408"
+};
+
+// Initialize your Firebase app
+firebase.initializeApp(config);
+
+// Reference to the comment's object in your Firebase database
+var comments = firebase.database().ref("comments");
+
+
+// Save a new comment to the database, using the input in the form
+var submitComment = function () {
+
+  var title = $("#yourName").val();
+  var remark = $("#yourComment").val();
+  var link = $("#yourLink").val();
+
+  comments.push({
+    "title": title,
+    "remark": remark,
+    "link": link
+  });
+};
+
+
+comments.limitToLast(10).on('child_added', function(childSnapshot) {
+
+  comment = childSnapshot.val();
+
+  $("#title").prepend(comment.title + '<br> <br>')
+  $("#remark").prepend(comment.remark + '<br> <br>')
+  $("#link").prepend('<img id="image">' + '<br> <br>')
+
+  
+  var altWords = 'No \n Image'  
+  // Make the link actually work and direct to the URL provided
+  $("#image").attr("src", comment.link)
+  $("#image").attr("alt", altWords) 
+});
+
+
+// When the window is fully loaded, call this function.
+$(window).load(function () {
+  $("#commentForm").submit(submitComment);
+});
